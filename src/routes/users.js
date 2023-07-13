2b2794486900d957a8c98aff61156ea424340ce4
mwhitham112 <134304879+mwhitham112@users.noreply.github.com>
2023-07-13 14:52:02 +0100

    Delete src directory

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/userController.js');
-
-router.get('/', usersController.getUsers);
-router.get('/:username', usersController.getUserByID);
-router.post('/newuser', usersController.createUser);
-
-module.exports = router;
